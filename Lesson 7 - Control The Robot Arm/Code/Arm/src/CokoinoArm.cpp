840f9eb89aff1ea570f8b995563f71f6fac0a063
Fiona Li <fl.shoyuramen@gmail.com>
2025-07-21 09:14:29 -0700

    Update CokoinoArm.cpp

--- Lesson 7 - Control The Robot Arm/Code/Arm/src/CokoinoArm.cpp
+++ Lesson 7 - Control The Robot Arm/Code/Arm/src/CokoinoArm.cpp
@@ -8,30 +8,37 @@ void CokoinoArm::JoyStickAttach(uint8_t xpin1,uint8_t ypin1,uint8_t xpin2,uint8_
 } 
 CokoinoArm::CokoinoArm(){}
 /////////////////////////////////////////////////////////
-void CokoinoArm::ServoAttach(uint8_t servo1_pin,uint8_t servo2_pin,uint8_t servo3_pin,uint8_t servo4_pin)
+void CokoinoArm::ServoAttach(uint8_t servo1_pin,uint8_t servo2_pin,uint8_t servo3_pin,uint8_t servo4_pin,uint8_t servo5_pin)
 {
   servo1.attach(servo1_pin);
   servo2.attach(servo2_pin);
   servo3.attach(servo3_pin);
   servo4.attach(servo4_pin);
+  servo5.attach(servo5_pin);
   angle[0]=servo1.read();
   angle[1]=servo2.read();
   angle[2]=servo3.read();
   angle[3]=servo4.read();
+  angle[4]=servo5.read();
 }
 /////////////////////////////////////////////////////////
 void CokoinoArm::up(int speed)
 {
   angle[1]=servo2.read();
   angle[2]=servo3.read();
+  angle[4]=servo5.read();
   angle[1]=angle[1]+1;
   angle[2]=angle[2]-1;
+  angle[4]=angle[4]+1;
   if(angle[1]>=180)
     {angle[1]= 180;}
   if(angle[2]<=0)
     {angle[2]= 0;}
+  if (angle[4] >= 180) 
+    {angle[4] = 180;}
   servo2.write(angle[1]);
   servo3.write(angle[2]);
+  servo5.write(angle[4]);
   delay(speed);      
 }
 /////////////////////////////////////////////////////////
@@ -39,14 +46,19 @@ void CokoinoArm::down(int speed)
 {
   angle[1]=servo2.read();
   angle[2]=servo3.read();
+  angle[4]=servo5.read();
   angle[1]=angle[1]-1;
   angle[2]=angle[2]+1;
+  angle[4]=angle[4]-1;
   if(angle[1]<=0)
     {angle[1] = 0;}
   if(angle[2]>=180)
     {angle[2] =180;}
+  if(angle[4]<=0)
+    {angle[4] = 0;}
   servo2.write(angle[1]);
   servo3.write(angle[2]);
+  servo5.write(angle[4]);
   delay(speed);    
 }
 /////////////////////////////////////////////////////////
@@ -98,9 +110,9 @@ int *CokoinoArm::captureAction(void)
 void CokoinoArm::do_action(int *angle,int speed)
 {
   int count=0;
-  int T_angle[4];
-  int S_angle[4];
-  for(int i=0;i<4;i++){
+  int T_angle[5];
+  int S_angle[5];
+  for(int i=0;i<5;i++){
 	T_angle[i]= *angle;
 	angle++;
     }
@@ -108,15 +120,18 @@ void CokoinoArm::do_action(int *angle,int speed)
   S_angle[1]=servo2.read();
   S_angle[2]=servo3.read();
   S_angle[3]=servo4.read();
+  S_angle[4]=servo5.read();
   do{
 	S_angle[0] = (T_angle[0]>S_angle[0])?S_angle[0]+1:S_angle[0]-1;
 	servo1.write(S_angle[0]);
-    S_angle[1] = (T_angle[1]>S_angle[1])?S_angle[1]+1:S_angle[1]-1;
-    servo2.write(S_angle[1]);
+        S_angle[1] = (T_angle[1]>S_angle[1])?S_angle[1]+1:S_angle[1]-1;
+        servo2.write(S_angle[1]);
 	S_angle[2] = (T_angle[2]>S_angle[2])?S_angle[2]+1:S_angle[2]-1;
 	servo3.write(S_angle[2]);
 	S_angle[3] = (T_angle[3]>S_angle[3])?S_angle[3]+1:S_angle[3]-1;
 	servo4.write(S_angle[3]);
+	S_angle[4] = (T_angle[4] > S_angle[4]) ? S_angle[4] + 1 : S_angle[4] - 1;
+	servo5.write(S_angle[4]);
 	count++;
 	delay(speed);
   }while(count<180);
